c0d909679fcebb4cc77f36782aca1e4045ea586f
Arslan05111 <Arslanhabibov010@gmail.com>
2022-09-28 12:35:22 +0200

    Quellcode wurde erstellt, Dialogwörterbuch wurde bearbeitet

--- Chatbot-Team_11.py
+++ Chatbot-Team_11.py
@@ -2,30 +2,30 @@ import random
 
 zufallsantworten = ["Bitte wählen Sie eine der oberen Optionen"]
 
-reaktionsantworten = {"computer/laptops": "Wählen Sie eine der folgenden Optionen: Störung-Laptop, Störung-Peripherie, Störung-Lan/WLAN, Installation/Deinstallation",
+reaktionsantworten = {"1": "Wählen Sie eine der folgenden Optionen: Störung-Laptop, Störung-Peripherie, Störung-Lan/WLAN, Installation/Deinstallation",
                                         "störung-laptop":"Wählen Sie eine der folgende Probleme: Störung-Stromversorgung, ",
                                         "störung-peripherie":"Test",
                                         "störung-lan/wlan": "Test",
                                         "installation/deinstallation": "Test",
-                      "mobile-geräte":"Wählen Sie eine der folgenden Optionen: Störung-Smartphone/Tablet, Verlust-Smarthpone/Tablet",
+                      "2":"Wählen Sie eine der folgenden Optionen: Störung-Smartphone/Tablet, Verlust-Smarthpone/Tablet",
                                         "störung-smartphone/tablet": "Test",
                                         "verlust-smarthpone/tablet": "Test",
-                      "telefonie":"Wählen Sie eine der folgenden Optionen: Desktop-Phone/Support, Neuen-Durschwahl",
+                      "3":"Wählen Sie eine der folgenden Optionen: Desktop-Phone/Support, Neuen-Durschwahl",
                                         "desktop-phone/support": "Test",
                                         "neuen-durschwahl": "Test",
-                      "drucken/scannen": "Wählen Sie eine der folgenden Optionen: Störung-Drucker, Druckerzubehör-bestellen",
+                      "4": "Wählen Sie eine der folgenden Optionen: Störung-Drucker, Druckerzubehör-bestellen",
                                         "störung-drucker": "Test",
                                         "druckerzubehör-bestellen": "Test",
-                      "bestellung": "Wählen Sie eine der folgenden Optionen: IT-Bestellungen",
+                      "5": "Wählen Sie eine der folgenden Optionen: IT-Bestellungen",
                                         "it-bestellungen": "Test",
-                      "e-mail": "Wählen Sie eine der folgenden Optionen: Störung-Outlook, Störung-Versand/Empfang, Störung-Mailarchiv, Verteilerliste",
+                      "6": "Wählen Sie eine der folgenden Optionen: Störung-Outlook, Störung-Versand/Empfang, Störung-Mailarchiv, Verteilerliste",
                                         "störung-outlook": "Test",
                                         "störung-versand/empfang": "Test",
                                         "störung-mailarchiv": "Test",
                                         "verteilerliste": "Test",
-                      "sicherheit": "Wählen Sie eine der folgenden Optionen: Schadsoftware/Betrugsattacken",
+                      "7": "Wählen Sie eine der folgenden Optionen: Schadsoftware/Betrugsattacken",
                                         "schadsoftware/betrugsattacken": "Test",
-                      "support.kontakt": "Wir sind von 9:00 bis 15:00 unter 49-000-00-00 erreischbar."
+                      "8": "Wir sind von 9:00 bis 15:00 unter 49-000-00-00 erreischbar."
 
                      }
 
@@ -33,9 +33,9 @@ print("Hallo und willkommen zu unserem digitalen Assistenten.")
 print("Mit was kann ich heute für Sie behilflich sein?")
 print("Bitte wählen Sie eine der folgenden Optionen: ")
 print("")
-print("|", "Computer/Laptops", "|", "Mobile-Geräte", "|", "Telefonie", "|", "Drucken/Scannen", "|", "Bestellung",
+print("|", "1-Computer/Laptops", "|", "2-Mobile-Geräte", "|", "3-Telefonie", "|", "4-Drucken/Scannen", "|", "5-Bestellung",
       "|")
-print("|", "Support.Kontakt", "|", "E-Mail", "|", "Sicherheit", "|")
+print("|", "6-Support.Kontakt", "|", "7-E-Mail", "|", "8-Sicherheit", "|")
 print("")
 print("Zum beenden bitte 'bye' eintippen")
 print("")
