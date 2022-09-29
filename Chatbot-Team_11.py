cd674cfe389420239a5835715f52bb15b9541f0d
Arslan05111 <Arslanhabibov010@gmail.com>
2022-09-29 09:41:37 +0200

    Quellcode wurde bearbeitet und letzte schritte werden Überpruft. Final Point

--- Chatbot-Team_11.py
+++ Chatbot-Team_11.py
@@ -3,35 +3,46 @@ import random
 zufallsantworten = ["Bitte wählen Sie eine der oberen Optionen"]
 
 reaktionsantworten = {"1": "Wählen Sie eine der folgenden Optionen: Störung-Laptop, Störung-Peripherie, Störung-Lan/WLAN, Installation/Deinstallation",
-                                        "störung-laptop":"Wählen Sie eine der folgende Probleme: Störung-Stromversorgung, ",
-                                        "störung-peripherie":"Test",
-                                        "störung-lan/wlan": "Test",
-                                        "installation/deinstallation": "Test",
+                                        "störung-laptop":"Wählen Sie eine der folgende Probleme: Windows-hängt, Strom-Störung, Hardware-Defekt ",
+                                                        "windows-hängt": "Die Ursache dafür sind die veraltete Treiber, die nicht kompatible mit Programmen sind.Ob Treiber-Probleme vorliegen, können Sie im Geräte-Manager prüfen. Außerdem empfehlen wir, nach Treiber-Aktualisierungen zu suchen und die neusten Windows-Updates zu installieren.",
+                                                        "strom-störung": "Überprüfen Sie die Funktion des Netzgerätes,Das Notebook schaltet sich während des Betriebs plötzlich aus:Hier sollte man die Strombuchse oder das Netzteil überprüfen",
+                                                        "hardware-defekt": "Bitte beschreiben Sie kurz per email: solutions.it@gmail.com, was genau bei Ihrem Gerät beschädigt wurde. Information wird weiter an unseren Techniker geleitet.",
+                                        "störung-peripherie":"Wählen Sie eine der folgende Probleme: Hardware-Defekt, Display-Fehlermeldung, Hardware-Bestellung",
+                                                        "hardware-defekt": "Bitte beschreiben Sie kurz per email: solutions.it@gmail.com, was genau bei Ihrem Gerät beschädigt wurde. Information wird weiter an unseren Techniker geleitet.",
+                                                        "display-fehlermeldung": "Prüfen Sie den Anschlussmodus, Anzeige „Unbekannter Monitor“: installieren Sie den vom Monitorhersteller bereit gestellten Treiber",
+                                                        "hardware-bestellung": "Bitte besuchen Sie unsere Onlineseite unter www.solutions.it.shop.de/search und schicken Sie uns die ausgewählte Hardware, mit entsprechendem Link und Kostenstelle",
+                                        "störung-lan/wlan":"Wählen Sie eine der folgende Probleme: Schlechte-Empfang, WLAN-nicht-gefunden",
+                                                        "schlechte-empfang": "den Router ausschalten und ein paar Minuten später neu starten., .Stellen Sie sicher, dass die neueste Version der Software installiert ist.",
+                                                        "wlan-nicht-gefunden": "Stellen Sie dabei unbedingt sicher, dass die WLAN-Funktion am Router eigeschaltet ist und dass Sie sich mit Ihrem Computer in der Reichweite des WLAN-Signals befinden. ",
+                                        "installation/deinstallation": "Folgende Iformation wird für uns benötigt werden, Allgemeine-Software oder Lizenspflichtige-Software, name der Gerätes und schicken Sie uns ein E-mail: soluitons.it@gmail.com",
                       "2":"Wählen Sie eine der folgenden Optionen: Störung-Smartphone/Tablet, Verlust-Smarthpone/Tablet",
-                                        "störung-smartphone/tablet": "Test",
-                                        "verlust-smarthpone/tablet": "Test",
+                                        "störung-smartphone/tablet": "Wählen Sie eine der folgende Probleme: Gerät-gesperrt, Hardware-Defekt",
+                                                        "gerät-gesperrt": "Entsperren kann man das Handy dann, indem man auf die Webseite des Geräte Managers geht und die Google-Kontodaten eingibt. Dort findet ihr die Option Sperren und könnt ein neues Passwort erstellen." ,
+                                                        "hardware-defekt": "Bitte beschreiben Sie kurz per email: solutions.it@gmail.com, was genau bei Ihrem Gerät beschädigt wurde. Information wird weiter an unseren Techniker geleitet.",
+                                        "verlust-smarthpone/tablet": "Bitte schicken Sie die folgende Information (Name, Modelle Ihres Gerätes und kurze Beschreibung dafür) an uns per E-Mail solutions.it@gmail.com. Achtung, SIM-Karte wird ab jetzt gesperrt.",
                       "3":"Wählen Sie eine der folgenden Optionen: Desktop-Phone/Support, Neuen-Durschwahl",
-                                        "desktop-phone/support": "Test",
-                                        "neuen-durschwahl": "Test",
+                                        "desktop-phone/support": "Bitte kontaktieren Sie unsere Support unter +49111111111, damit wir das Problem einmal vor Ort anschauen würden.",
+                                        "neuen-durschwahl": "Folgende Iformation wird für uns benötigt werden: Name, der der Durchwahl hinterlegt werden soll und Ort Ihres Arbeitsplatzes. Unsere Kollege werden sich mit Ihnen kontaktieren. ",
                       "4": "Wählen Sie eine der folgenden Optionen: Störung-Drucker, Druckerzubehör-bestellen",
-                                        "störung-drucker": "Test",
-                                        "druckerzubehör-bestellen": "Test",
+                                        "störung-drucker": "Überprüfen Sie zuerst, ob der Drucker korrekt an Ihren Computer angeschlossen ist.",
+                                        "druckerzubehör-bestellen": "Folgende Iformation wird für uns benötigt werden: DEP, Seriennummer oder IP Adresse und welche Tonner farben benötigt werden.",
                       "5": "Wählen Sie eine der folgenden Optionen: IT-Bestellungen",
-                                        "it-bestellungen": "Test",
+                                        "it-bestellungen": "Bitte besuchen Sie unsere Onlineseite unter www.solutions.it.shop.de/search und schicken Sie uns die ausgewählte Hardware, mit entsprechendem Link und Kostenstelle",
                       "6": "Wählen Sie eine der folgenden Optionen: Störung-Outlook, Störung-Versand/Empfang, Störung-Mailarchiv, Verteilerliste",
-                                        "störung-outlook": "Test",
-                                        "störung-versand/empfang": "Test",
-                                        "störung-mailarchiv": "Test",
-                                        "verteilerliste": "Test",
+                                        "störung-outlook": "Wählen Sie in Outlook 2016 die Option Datei aus. Wählen Sie Kontoeinstellungen > Kontoeinstellungen aus. Wählen Sie auf der Registerkarte E-Mail Ihr Konto (Profil) und dann Reparieren aus.",
+                                        "störung-versand/empfang": "Um dies zu überprüfen, gehen Sie zu den Kontoeinstellungen oder fügen Sie Ihr Konto erneut hinzu. Vergewissern Sie sich, dass die E-Mail-Adresse und das Passwort korrekt sind.",
+                                        "störung-mailarchiv": "Bitte nutzen Sie die Online Vault als ersten Workaround. Wir werden Sie zur Behebung des Problems schnellstmöglich kontaktieren.",
+                                        "verteilerliste": "Folgende Iformation wird für uns benötigt werden: Name für das Verteiler, wer als Owner eingesetzt werden soll. Unsere Kollegen werden weiter informiert.",
                       "7": "Wählen Sie eine der folgenden Optionen: Schadsoftware/Betrugsattacken",
-                                        "schadsoftware/betrugsattacken": "Test",
+                                        "schadsoftware/betrugsattacken": "Wählen Sie eine der folgende Probleme: Spam/Fisching",
+                                                        "spam/fisching": "Spam-Mails sind unerwünschte E-Mails (z.B. Werbemails). Diese können Schadpogramme enthalten (z.B. gefälschte Bewerbung). Phishing-Mails zielen darauf ab vertrauliche Informationen zu stehlen, indem Sie Anhänge oder Links auf gefährliche Webseiten enthalten.",
                       "8": "Wir sind von 9:00 bis 15:00 unter 49-000-00-00 erreischbar."
 
                      }
 
 print("Hallo und willkommen zu unserem digitalen Assistenten.")
 print("Mit was kann ich heute für Sie behilflich sein?")
-print("Bitte wählen Sie eine der folgenden Optionen: ")
+print("Bitte wählen Sie eine Nummer der folgenden Optionen: ")
 print("")
 print("|", "1-Computer/Laptops", "|", "2-Mobile-Geräte", "|", "3-Telefonie", "|", "4-Drucken/Scannen", "|", "5-Bestellung",
       "|")
@@ -59,4 +70,4 @@ while nutzereingabe != "bye":
         
     print("")
 
-print("Einen schönen Tag wünsche ich Dir. Bis zum nächsten Mal")
+print("Wir wünschen Ihnen noch einen schönen Tag. Bis zum nächsten Mal")
